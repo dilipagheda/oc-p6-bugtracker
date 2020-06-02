8926761182b28d3eb32ee737ca9719a667036192
Dilip Agheda <dilip_agheda@yahoo.com>
2020-06-02 20:23:15 +1000

    Added DbContext class along with Seeding data

---
+++ BugTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using BugTracker.Data.Models;
+using BugTracker.Data.Utils;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OperatingSystem = BugTracker.Data.Models.OperatingSystem;
+using Version = BugTracker.Data.Models.Version;
+
+namespace BugTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly int MAX_RECORDS = 50;
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        public virtual DbSet<Version> Versions { get; set; }
+
+        public virtual DbSet<ProductOSVersion> ProductOSVersions { get; set; }
+
+        public virtual DbSet<OperatingSystem> OperatingSystems { get; set; }
+
+        public virtual DbSet<IssueStatus> IssueStatusList { get; set; }
+
+        public virtual DbSet<Issue> Issues { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext() { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ProductOSVersion>()
+                .HasAlternateKey(x => new { x.ProductId, x.OperatingSystemId, x.VersionId });
+
+            //Seed Data
+            builder.Entity<Product>().HasData(FakeDataUtils.GetFakeProducts(MAX_RECORDS));
+            builder.Entity<Version>().HasData(FakeDataUtils.GetFakeVersions(MAX_RECORDS));
+
+            builder.Entity<OperatingSystem>().HasData(FakeDataUtils.GetFakeOperatingSystems());
+            builder.Entity<IssueStatus>().HasData(FakeDataUtils.GetFakeIssueStatusList());
+            var rangeConfig = new RangeConfigProductOSVersion()
+            {
+                MinProductId = 1,
+                MaxProductId = MAX_RECORDS,
+                MinOperatingSystemId = 1,
+                MaxOperatingSystemId = FakeDataUtils.GetCountOfOperatingSystem(),
+                MinVersionId = 1,
+                MaxVersionId = MAX_RECORDS
+            };
+            builder.Entity<ProductOSVersion>().HasData(FakeDataUtils.GetFakeProductOsVersions(MAX_RECORDS, rangeConfig));
+            var rangeConfigIssue = new RangeConfigIssue()
+            {
+                MinIssueStatusId = 1,
+                MaxIssueStatusId = FakeDataUtils.GetCountOfIssueStatusList(),
+                MinProductOSVersionId = 1,
+                MaxProductOSVersionId = MAX_RECORDS
+            };
+            builder.Entity<Issue>().HasData(FakeDataUtils.GetFakeIssues(MAX_RECORDS, rangeConfigIssue));
+        }
+    }
+}
