ac6067ab3a9c7f278b1f8a43378fb1bbb9ad31b7
Dilip Agheda <dilip_agheda@yahoo.com>
2020-06-02 20:22:16 +1000

    Created Model classes for Entity Framework

---
+++ BugTracker/Data/Models/Issue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+
+        public int ProductOSVersionId { get; set; }
+
+        public ProductOSVersion ProductOSVersion { get; set; }
+
+        public int IssueStatusId { get; set; }
+
+        public IssueStatus IssueStatus { get; set; }
+
+        public string Description { get; set; }
+
+        public string Resolution { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? ResolutionDate { get; set; }
+    }
+}
---
+++ BugTracker/Data/Models/ProductOSVersion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data.Models
+{
+    public class ProductOSVersion
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public int OperatingSystemId { get; set; }
+
+        public OperatingSystem OperatingSystem { get; set; }
+
+        public int VersionId { get; set; }
+
+        public Version Version { get; set; }
+    }
+}
