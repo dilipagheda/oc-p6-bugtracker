2081f7753baa86095d4c10d16ac9b7da8e0737cf
Dilip Agheda <dilip_agheda@yahoo.com>
2020-06-04 18:40:11 +1000

    Added ViewModel class for Issues

---
+++ BugTracker/Data/ViewModels/IssueViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data.ViewModels
+{
+    [NotMapped]
+    public class IssueViewModel
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string VersionName { get; set; }
+
+        public string OSName { get; set; }
+
+        public string Status { get; set; }
+
+        public string Description { get; set; }
+
+        public string Resolution { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ResolutionDate { get; set; }
+    }
+}
