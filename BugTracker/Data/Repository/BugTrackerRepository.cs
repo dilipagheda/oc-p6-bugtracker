749420b37cbfb732aebc9eb51a9823cf871dd1fa
Dilip Agheda <dilip_agheda@yahoo.com>
2020-06-04 18:39:57 +1000

    Added BugTracker Repository

---
+++ BugTracker/Data/Repository/BugTrackerRepository.cs
@@ -0,0 +1,44 @@
+﻿using BugTracker.Data.Models;
+using BugTracker.Data.ViewModels;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data.Repository
+{
+    public class BugTrackerRepository : IBugTrackerRepository
+    {
+        private ApplicationDbContext _dbContext;
+        private readonly IConfiguration _config;
+
+        public BugTrackerRepository(ApplicationDbContext dbContext, IConfiguration config)
+        {
+            _dbContext = dbContext;
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get { return new SqlConnection(_config.GetConnectionString("DefaultConnection")); }
+        }
+
+        public List<IssueViewModel> GetAllIssuesByStatusAsync(string status)
+        {
+            using(IDbConnection conn = Connection)
+            {
+                conn.Open();
+                var issues = conn.Query<IssueViewModel>("GET_ALL_ISSUES_BY_STATUS",
+                                                        new { Status = status },
+                                                        commandType: CommandType.StoredProcedure)
+                    .ToList();
+                return issues;
+            }
+        }
+    }
+}
---
+++ BugTracker/Data/Repository/IBugTrackerRepository.cs
@@ -0,0 +1,16 @@
+﻿using BugTracker.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data.Repository
+{
+    public interface IBugTrackerRepository
+    {
+        List<IssueViewModel> GetAllIssuesByStatusAsync(string status);
+
+        IDbConnection Connection { get; }
+    }
+}
