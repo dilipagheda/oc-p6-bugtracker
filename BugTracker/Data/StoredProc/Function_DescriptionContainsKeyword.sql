1d9b8e60bbce27fd98d151e932f88c9753181538
Dilip Agheda <dilip_agheda@yahoo.com>
2020-06-07 14:48:36 +1000

    Added a SQL Server function to check if a description text contains a list of keywords in an arbitary order

---
+++ BugTracker/Data/StoredProc/Function_DescriptionContainsKeyword.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION DescriptionContainsKeywords (@desc VARCHAR(max), @keywords nvarchar(max))
+RETURNS int
+AS BEGIN
+    DECLARE @MatchCount int
+	DECLARE @TotalKeyWords int
+
+	set @MatchCount = (select count(a.value) from 
+	(select distinct value from string_split(@desc,' ')) a,
+	(select distinct value from string_split(@keywords, ';')) b
+	where REPLACE(a.value,'.','') = b.value)
+
+	select @TotalKeyWords = (select count(a.value) from (select value from string_split(@keywords, ';')) a)
+
+
+    RETURN @MatchCount - @TotalKeyWords
+END
